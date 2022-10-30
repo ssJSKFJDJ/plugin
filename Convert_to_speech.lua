508c592b642c9c7a0363dfafde87e02530964f22
Hsiang Nianian <Hsiangnianian@outlook.com>
2022-10-30 10:11:29 +0800

    Update Convert_to_speech.lua
    改了改那么丑的缩进

--- Convert_to_speech.lua
+++ Convert_to_speech.lua
@@ -1,15 +1,19 @@
-msg_order={}
-     function record(msg)
-     local rest = string.match(msg.fromMsg,"^转语音:(.+)$")
-     if(rest == nil)then
-     return ""
-     else
-     if(msg.gid ~= nil)then
-     local api = "https://api.vvhan.com/api/song?txt="..rest.."&per=3"
-     return "[CQ:record,url="..api.."]"
-     else
-     return"{nick}，目前本功能不支持私聊哦~"
-     end
-     end
-     end
-     msg_order["转语音:"]="record"
+----------------------------
+-- @转语音 by zmsv
+-- @license MIT.
+----------------------------
+msg_order = {}
+function record(msg)
+    local rest = string.match(msg.fromMsg, "^转语音:(.+)$")
+    if (rest == nil) then
+        return ""
+    else
+        if (msg.gid ~= nil) then
+            local api = "https://api.vvhan.com/api/song?txt=" .. rest .. "&per=3"
+            return "[CQ:record,url=" .. api .. "]"
+        else
+            return "{nick}，目前本功能不支持私聊哦~"
+        end
+    end
+end
+msg_order["转语音:"] = "record"
